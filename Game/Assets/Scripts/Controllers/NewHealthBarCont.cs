81f59f49f8c39cabd6adf84a89a82786fe482160
SOFTSAM <sam96mel95@gmail.com>
2017-08-16 12:18:01 +0300

    Final touch about health bars

---
+++ Game/Assets/Scripts/Controllers/NewHealthBarCont.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewHealthBarCont : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Image>().fillAmount=WorldController.Instance.world.character.health/100;
+	}
+}
