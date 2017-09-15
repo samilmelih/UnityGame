cdce5e6a5f6e9f2b564a03411bb296e6f0965a80
Melih <sam96mel95@gmail.com>
2017-09-15 22:33:27 +0300

    Leaved some TODO note to CameraController.

--- Game/Assets/Scripts/Controllers/CameraController.cs
+++ Game/Assets/Scripts/Controllers/CameraController.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	// TODO: We will have gameobjects on main camera's borders. They will interact with the
+	// checkpoint triggers. If we do this way, character doesn't need to interact with the
+	// checkpoint triggers. We just need to limit our character in main camera. That'll be enough.
+	// Why we do this way? If main character was interact with the cp trigs, we would not limit camera
+	// properly. Because our character is at center of the camera.
+
+	// How we gonna to this? We need to know where is our camera's borders. Top border is easy.
+	// We just need look at camera.orthographicSize but left and right is a little bit tricky.
+	// Because they change up to aspect ratio. We need to calculate it depend on aspect ratio.
+	// As a summary, we need to know how far left or right, we are gonna place our detector gameobjects.
+	// After calculated, we will give them to camera as a child and look if they triggered or not.
+
 	void Update()
 	{
 		GameObject go_mainCharacter = CharacterCont.Instance.go_mainCharacter;
